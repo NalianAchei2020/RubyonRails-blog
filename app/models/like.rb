3be468f1c68c2737f432ed2911700afdd292b49d
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2024-01-10 16:31:39 +0100

    Update like.rb

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 
 # The `Like` class represents the association between a user and a post indicating that the user has liked the post.
 # It is a join model that belongs to both a user and a post.
