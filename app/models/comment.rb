a0d9e445442aafc76bf352b32b01f0a592e7134c
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2024-01-10 17:05:03 +0100

    fix linters

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+
 
 # Represents a comment made by a user on a post.
 class Comment < ApplicationRecord
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+
 
 # The `Post` class represents a blog post in the application.
 # It belongs to an `author` who created the post.
