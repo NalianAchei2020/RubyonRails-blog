160e83bd16468e64e90e17b969d8456d010b4623
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2024-01-11 16:04:34 +0100

    add user migrate

--- db/migrate/20240110095949_create_users.rb
+++ db/migrate/20240110095949_create_users.rb
@@ -1,11 +1,12 @@
+# user migrate
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       t.string :name
       t.string :photo
       t.text :bio
-      t.datetime :updated_at
-      t.datetime :created_at
+      t.datetime :updatedAt
+      t.datetime :createdAt
       t.integer :posts_counter
 
       t.timestamps
