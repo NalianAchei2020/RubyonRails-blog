0b190b5879d6abc2c1a69f227af4119b54d2f2f5
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2024-01-12 10:19:44 +0100

    add posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,13 @@
 class Post < ApplicationRecord
-  belongs_to :author
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
+
+  def update_posts_counter
+    update(posts_counter: comments.count)
+  end
+
+  def recent_comments(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
+  end
 end
