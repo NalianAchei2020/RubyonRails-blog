8abe7e392dc183b9e104754259059bd77db9328e
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2024-01-17 09:48:17 +0100

    Create postController.rb

---
+++ app/controllers/postController.rb
@@ -0,0 +1,12 @@
+class PostsController < ApplicationController
+    def index
+      # Retrieve all posts by the user with the specified user_id from the database
+      @user = User.find(params[:user_id])
+      @posts = @user.posts
+    end
+  
+    def show
+      # Retrieve the post with the specified id from the database
+      @post = Post.find(params[:id])
+    end
+  end
