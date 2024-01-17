f1b33ee8feb69968f550f14bc45fdf10d3f01025
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2024-01-17 09:51:40 +0100

    fix linter errors

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-    get '/users', to: 'users#index'
+  get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show', as: 'user'
-    get '/users/:user_id/posts', to: 'posts#index'
-    get '/posts/:id', to: 'posts#show'
+  get '/users/:user_id/posts', to: 'posts#index'
+  get '/posts/:id', to: 'posts#show'
 end
