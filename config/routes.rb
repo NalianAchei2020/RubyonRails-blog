2837fec8edf74e52a1a90c54718efdd424f567e3
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2024-01-17 10:26:02 +0100

    make changes as required

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   # root "posts#index"
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show', as: 'user'
-  get '/users/:user_id/posts', to: 'posts#index'
+  get '/users/:user_id/posts/:id', to: 'posts#show'
   get '/posts/:id', to: 'posts#show'
 end
