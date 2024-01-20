ce357d469f51661245a06d49429983a20bc2a954
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2024-01-20 08:54:03 +0100

    make changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   def index
+    # Retrieve all users from the database
     @users = User.all
   end
 
   def show
+    # Retrieve the user with the specified id from the database
     @user = User.find(params[:id])
-    @recent_posts = @user.recent_posts
   end
 end
